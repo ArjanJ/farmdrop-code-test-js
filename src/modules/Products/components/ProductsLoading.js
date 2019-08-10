c3cdd95471c31d624ed6eaf6ce067d6829e8d416
Arjan Jassal <arjan.jassal.27@gmail.com>
2019-08-10 10:59:47 -0700

    move Spinner into own folder

--- src/modules/Products/components/ProductsLoading.js
+++ src/modules/Products/components/ProductsLoading.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 
 import { colors } from '../../../styles/colors';
+import { Spinner } from '../../Spinner/Spinner';
 
 export const ProductsLoading = () => (
   <ProductsLoadingWrapper>
@@ -22,32 +23,3 @@ const ProductsLoadingText = styled.span`
   color: ${colors.DARK_GREEN};
   margin-right: 8px;
 `;
-
-const rotate = keyframes`
-  0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
-`;
-
-const Spinning = styled.svg`
-  animation: 1s ${rotate} linear infinite;
-`;
-
-const Spinner = ({ color, size, stroke = 5 }) => (
-  <Spinning width={size} height={size}>
-    <defs>
-      <linearGradient id="grad1" x1="0" y1="1" x2="1" y2="0">
-        <stop offset="0" stopColor="#FFFFFF" />
-        <stop offset="1" stopColor={color} />
-      </linearGradient>
-    </defs>
-    <g fill="none">
-      <circle
-        cx={size / 2}
-        cy={size / 2}
-        r={size / 2 - 5}
-        stroke="url(#grad1)"
-        strokeWidth={stroke}
-      />
-    </g>
-  </Spinning>
-);
---
+++ src/modules/Spinner/Spinner.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const rotate = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`;
+
+const Spinning = styled.svg`
+  animation: 1s ${rotate} linear infinite;
+`;
+
+export const Spinner = ({ color, size, stroke = 5 }) => (
+  <Spinning width={size} height={size}>
+    <defs>
+      <linearGradient id="grad1" x1="0" y1="1" x2="1" y2="0">
+        <stop offset="0" stopColor="#FFFFFF" />
+        <stop offset="1" stopColor={color} />
+      </linearGradient>
+    </defs>
+    <g fill="none">
+      <circle
+        cx={size / 2}
+        cy={size / 2}
+        r={size / 2 - 5}
+        stroke="url(#grad1)"
+        strokeWidth={stroke}
+      />
+    </g>
+  </Spinning>
+);
